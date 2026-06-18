ba3fb6779c543db88a2811492daabf501ca477da
Nishant More <nishantmore228@gmail.com>
2026-06-18 21:00:16 +0530

    refactor: Add start server handler

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import cookieParser from 'cookie-parser';
 import { logger } from './utils/logger.js';
 import { httpLogger } from './middlewares/httpLogger.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { sql } from 'drizzle-orm';
+import { db } from './db/index.js';
 
 dotenv.config();
 
@@ -17,8 +19,16 @@ app.use(httpLogger);
 app.use(express.json());
 app.use(cookieParser());
 
-
+// Routes
 app.use('/api/auth', authRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200)
+        .json({
+            status: "success",
+            message: "welcome to auth starter"
+        })
+})
 
 app.get('/health', (req: Request, res: Response) => {
     res.status(200).json({
@@ -27,8 +37,24 @@ app.get('/health', (req: Request, res: Response) => {
     })
 })
 
+// use global error handler
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    // 1. Ping the database to ensure it is actively accepting connections
+    await db.execute(sql`SELECT 1`);
+    logger.info('📦 Database connected successfully');
+
+    // 2. Start the Express server ONLY if the database is ready
+    app.listen(PORT, () => {
+      logger.info(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    // 3. Fail-Fast: Log the error and shut down the Node process
+    logger.error('❌ Database connection failed. Shutting down server...', error);
+    process.exit(1);
+  }
+};
+
+startServer();
